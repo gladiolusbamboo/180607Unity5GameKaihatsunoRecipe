4b8f4837e6f317d056cda0252bc38043a92eb3de
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-06-10 22:19:30 +0900

    180610 Main Cameraの親を作るには

---
+++ Chapter1/Assets/Scripts/PlayerRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    // プレイヤーを回転させる
+    transform.Rotate(0, Input.GetAxis("Horizontal2"), 0);
+
+    // 親オブジェクト（プレイヤーの中心）を中心にカメラを回転させる
+    GameObject CameraPoint = Camera.main.transform.parent.gameObject;
+    CameraPoint.transform.Rotate(Input.GetAxis("Vertical2"), 0, 0);
+  }
+}
