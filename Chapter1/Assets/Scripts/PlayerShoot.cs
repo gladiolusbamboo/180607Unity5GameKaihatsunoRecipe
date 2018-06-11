9adf5822d3266eed59d135461f0fb70a92431cd2
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-06-11 15:24:25 +0900

    180611 弾を発射するには

---
+++ Chapter1/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+  void Start()
+  {
+
+  }
+
+  public GameObject shot;
+
+  void Update()
+  {
+    // 弾を発射する
+    if (Input.GetButton("Fire1"))
+    {
+      Instantiate(shot, transform.position, transform.rotation);
+    }
+  }
+}
---
+++ Chapter1/Assets/Scripts/ShotPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotPlayer : MonoBehaviour
+{
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // 弾を前進させる
+    transform.position += transform.forward * Time.deltaTime * 100;
+  }
+}
